f6a937e746eec63bd1d30063106c25eae72b82fa
Karen Barcelos <karenlbarcelos@hotmail.com>
2024-04-01 17:29:12 -0300

    adding mysql depencies using docker

--- src/main/java/com/firstprojectsb/course/controller/CourseController.java
+++ src/main/java/com/firstprojectsb/course/controller/CourseController.java
@@ -3,11 +3,10 @@ package com.firstprojectsb.course.controller;
 import com.firstprojectsb.course.bean.Course;
 import com.firstprojectsb.course.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
@@ -35,4 +34,28 @@ public class CourseController {
         return course.get();
         //return new Course(1, "ADS", "Ranga");
     }
+
+    //POST - Create a new resource
+    @PostMapping("/courses")
+    public void createCourse(@RequestBody Course course){
+        courseRepository.save(course);
+    }
+
+    //PUT - Update a specific course
+    @PutMapping("/courses/{id}")
+    public void updateCourses(@RequestBody Course course, @PathVariable long id){
+        courseRepository.save(course);
+    }
+
+    //DELETE - Delete a specific course
+    @DeleteMapping("/courses/{id}")
+    public void deleteCourse(@PathVariable long id) {
+        if (courseRepository.findById(id).isEmpty()) {
+            throw new RuntimeException("Course not found with id " + id);
+        } else {
+            courseRepository.deleteById(id);
+        }
+
+    }
+
 }
